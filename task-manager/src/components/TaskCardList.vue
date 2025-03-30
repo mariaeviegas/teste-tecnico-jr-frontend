1ab93186aaa02c1766daaea2377487b1388a8a33
mariaeviegas <maria.viegas@sptech.school>
2025-03-30 14:00:34 -0300

    add: criação do card de informações da task e seções de status

---
+++ task-manager/src/components/TaskCardList.vue
@@ -0,0 +1,33 @@
+<script>
+import TaskInfoCard from './TaskInfoCard.vue'
+   export default {
+    components : {
+        TaskInfoCard
+    },
+    props: {
+        titleCard: {
+            type: String,
+            required: true
+        }
+    }
+   }
+</script>
+<template>
+    <div class="taskCardList">
+        <h1 class="taskCardList__title">{{ titleCard }}</h1>
+        <TaskInfoCard/>
+    </div>
+</template>
+<style>
+    .taskCardList {
+        width: 28%;
+        padding: 1.5rem;
+        background-color: white;
+        border-radius: 1rem;
+    }
+
+    .taskCardList__title {
+        font-size: 20px;
+        color: var(--primary-color);
+    }
+</style>
---
+++ task-manager/src/components/TaskInfoCard.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+<template>
+    <div class="taskInfoCard">
+        <div class="taskInfoCard__mainInfo">
+            <h1 class="mainInfo__title">ID - Titulo da tarefa</h1>
+            <!-- TODO: menu do car -->
+        </div>
+
+        <p class="taskInfoCard__description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat.
+        </p>
+
+        <p class="taskInfoCard__deadLine">25/05/2025</p>
+    </div>
+
+</template>
+<style scoped>
+
+.taskInfoCard {
+    height: max-content;
+    padding: 1rem;
+    background-color: var(--pending-status);
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+
+.mainInfo__title {
+    font-size: 18px;
+}
+
+.taskInfoCard__description {
+    display: -webkit-box;
+    -webkit-line-clamp: 3; 
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-weight: 200;
+    line-height: 1.5;
+}
+</style>
